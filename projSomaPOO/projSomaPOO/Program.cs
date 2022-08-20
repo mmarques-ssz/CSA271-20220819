3155afdda6492a93d9eb430e306420ec3f11b722
mmarques-ssz <37246763+mmarques-ssz@users.noreply.github.com>
2022-08-19 21:03:05 -0300

    Add files via upload

---
+++ projSomaPOO/projSomaPOO/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projSomaPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Soma soma1;
+            soma1 = new Soma();
+
+            Console.Write("Digite o 1º valor: ");
+            soma1.setN1(int.Parse(Console.ReadLine()));
+
+            Console.Write("Digite o 2º valor: ");
+            soma1.setN2(int.Parse(Console.ReadLine()));
+
+            soma1.somar();
+
+            Console.WriteLine("A soma de {0} com {1} é {2}", 
+                soma1.getN1(), soma1.getN2(), soma1.getResultado());
+
+        }
+    }
+}
---
+++ projSomaPOO/projSomaPOO/Soma.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projSomaPOO
+{
+    class Soma
+    {
+        private int n1;
+        private int n2;
+        private int resultado;
+
+        public void setN1(int n)
+        {
+            n1 = n;
+        }
+        public void setN2(int n)
+        {
+            n2 = n;
+        }
+        public int getN1()
+        {
+            return n1;
+        }
+        public int getN2()
+        {
+            return n2;
+        }
+        public int getResultado()
+        {
+            return resultado;
+        }
+        public void somar()
+        {
+            resultado = n1 + n2;
+        }
+    }
+}
